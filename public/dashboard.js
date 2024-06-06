41ab6468528d452d36376e7358677b5ada31b1d1
Colin Robinson <colindrums@me.com>
2024-06-06 14:44:49 -0400

    more mods

---
+++ public/dashboard.js
@@ -0,0 +1,21 @@
+document.getElementById('attire-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const weather = document.getElementById('weather').value.trim();
+    const eventDesc = document.getElementById('event').value.trim();
+    const suggestion = document.getElementById('suggestion').value.trim();
+
+    if (weather && eventDesc && suggestion) {
+        const response = await fetch('/api/attires', {
+            method: 'POST',
+            body: JSON.stringify({ weather, event: eventDesc, suggestion }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to add attire.');
+        }
+    }
+});
