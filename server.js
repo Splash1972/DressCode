e6287210a5d9fe3e906a4eaffc199201e17786c1
Colin Robinson <colindrums@me.com>
2024-06-10 11:08:06 -0400

    fixed label

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use(session({
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
