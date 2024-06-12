339726f4f98c3045e8551e1add78f9eb99c13344
Colin Robinson <colindrums@me.com>
2024-06-12 17:03:50 -0400

    fixed auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const withAuth = async (req, res, next) => {
     if (!req.session.loggedIn) {
         await req.session.destroy();
-        res.redirect('/login');
+        res.redirect('/homepage-attire');
     } else {
         next();
     }
